0af1b3d5e0398856918e51c71009a515d9d306b7
zulsb <zulsb2093@gmail.com>
2020-07-19 00:01:32 -0500

    Try one

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    This module contains the canUnlockAll function.
+"""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened
+        Arg:
+            boxes: Is a list of lists.
+    """
+    # li = []
+    # for i in range(len(boxes)):
+    #     for o in boxes[i]:
+    #         li.append(boxes[0])
+    # print(li, end=" ")
+
+    op_en = [False] * len(boxes)
+    op_en[0] = True
+    key = [0]       
+    while key:
+        lis = key.pop()
+        for i in boxes[lis]:
+            if not op_en[i]: 
+                op_en[i] = True 
+                key.append(i)
+    return all(op_en) 
