26cbd1a086410d5c850f509dd1a41343b5de1ab4
zulsb <zulsb2093@gmail.com>
2020-08-10 20:54:46 -0500

    Add a method that calculates minimum operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -12,15 +12,13 @@ def minOperations(n):
             n: Is a number to print H characters.
     """
     count = 0
-    
-    if n == 1:
-        return 0
-
-    while n != 1:
-        if n % 2 == 0:
-            n = n / 2
-        else:
-            n += 1
-            count += 2
-        count += 1
-    return count
+    if n:
+        while n != 1:
+            if n % 2 == 0:
+                n = n / 2
+            else:
+                n += 1
+                count += 2
+            count += 1
+        return count
+    return 0
