1a1b1511dc904a51d0f32db1766f889ca5d1f9d0
zulsb <zulsb2093@gmail.com>
2020-08-10 19:56:48 -0500

    Add a method that calculates minimum operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+    Contains the method that calculates minimum operations.
+"""
+
+
+def minOperations(n):
+    """ Method that calculates the fewest number of operations
+        needed to result in exactly n H characters in the file.
+        Args:
+            n: Is a number to print H characters.
+    """
+    count = 0
+    if n:
+        while n > 1:
+            if n % 2 == 0:
+                n = n / 2
+            else:
+                n += 1
+            count += 1
+        return count
+    return 0
