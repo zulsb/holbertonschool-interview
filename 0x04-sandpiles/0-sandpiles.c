72a924466db0567e311aaa3188a9bf5b15fe6149
zulsb <zulsb2093@gmail.com>
2020-08-16 23:42:43 -0500

    Add sandpiles sum function

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,23 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Function that computes the sum of two sandpiles.
+ * @grid1: Matrix 1 int type.
+ * @grid2: Matrix 2 int type.
+ *
+ * Return: nothing.
+*/
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int row = 0, column = 0;
+
+
+	for (row = 0; row < 3; row++)
+	{
+		for (column = 0; column < 3; column++)
+		{
+			grid1[row][column] = grid1[row][column] + grid2[row][column];
+		}
+	}
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,6 @@
+#ifndef SANDPILES_SUM
+#define SANDPILES_SUM
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_SUM */
