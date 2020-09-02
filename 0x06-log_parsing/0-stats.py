ddb4a429a75961dc1108a8070b84ee84b642509b
zulsb <zulsb2093@gmail.com>
2020-09-02 03:44:05 -0500

    Add log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,38 +2,37 @@
 """Module that reads stdin line by line and print metrics"""
 import sys
 
+stat = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+ctr = 0
+total_size = 0
+
+try:
+    for line in sys.stdin:
+        status_code = line.split('"')[2].split(" ")[1]
+        size = int(line.split('"')[2].split(" ")[2])
+        total_size += size
+        ctr += 1
+
+        for key in sorted(stat.keys()):
+            if status_code == key:
+                stat[key] += 1
+        if ctr == 10:
+            print("File size: {:d}".format(total_size))
+            for key in sorted(stat.keys()):
+                if stat[key]:
+                    print("{}: {:d}".format(key, stat[key]))
+            ctr = 0
+
+except KeyboardInterrupt:
+    print("File size: {:d}".format(total_size))
+    for key in sorted(stat.keys()):
+        if stat[key]:
+            print("{}: {:d}".format(key, stat[key]))
+    raise
 
 if __name__ == "__main__":
-
-    stat = {"200": 0, "301": 0, "400": 0, "401": 0,
-            "403": 0, "404": 0, "405": 0, "500": 0}
-    ctr = 0
-    total_size = 0
-
-    try:
-        for line in sys.stdin:
-            status_code = line.split('"')[2].split(" ")[1]
-            size = int(line.split('"')[2].split(" ")[2])
-            total_size += size
-            ctr += 1
-
-            for key in sorted(stat.keys()):
-                if status_code == key:
-                    stat[key] += 1
-            if ctr == 10:
-                print("File size: {:d}".format(total_size))
-                for key, v in sorted(stat.items()):
-                    if v > 0:
-                        print("{}: {:d}".format(key, stat[key]))
-                ctr = 0
-
-    except KeyboardInterrupt:
-        print("File size: {:d}".format(total_size))
-        for key, v in sorted(stat.items()):
-            if v > 0:
-                print("{}: {:d}".format(key, stat[key]))
-        raise
     print("File size: {:d}".format(total_size))
-    for key, v in sorted(stat.items()):
-        if v > 0:
+    for key in sorted(stat.keys()):
+        if stat[key]:
             print("{}: {:d}".format(key, stat[key]))
