af4fcb27da751f9915479f4b7f166a9210b03c8a
zulsb <zulsb2093@gmail.com>
2020-09-02 02:52:44 -0500

    Add log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,6 +7,7 @@ stat = {"200": 0, "301": 0, "400": 0, "401": 0,
 ctr = 0
 total_size = 0
 
+
 def prints():
     """Print metrics"""
     print("File size: ", total_size)
@@ -14,23 +15,23 @@ def prints():
         if stat[key]:
             print("{}: {:d}".format(key, stat[key]))
 
+try:
+    for line in stdin:
+        status_code = line.split('"')[2].split(" ")[1]
+        size = int(line.split('"')[2].split(" ")[2])
+        total_size += size
+        ctr += 1
+
+        for key in sorted(stat.keys()):
+            if status_code == key:
+                stat[key] += 1
+        if ctr == 10:
+            ctr = 0
+            prints()
+
+except KeyboardInterrupt:
+    prints()
+    raise
+prints()
 
 if __name__ == "__main__":
-    try:
-        for line in stdin:
-            status_code = line.split('"')[2].split(" ")[1]
-            size = int(line.split('"')[2].split(" ")[2])
-            total_size += size
-            ctr += 1
-
-            for key in sorted(stat.keys()):
-                if status_code == key:
-                    stat[key] += 1
-            if ctr == 10:
-                ctr = 0
-                prints()
-
-    except KeyboardInterrupt:
-        prints()
-        raise
-    prints()
