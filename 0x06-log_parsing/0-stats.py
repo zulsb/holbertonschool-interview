b56d685ef759c93ce5b790ad5e23c414dcfb7165
zulsb <zulsb2093@gmail.com>
2020-09-02 02:07:41 -0500

    git add readme and log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Script that reads stdin line by line and print metrics"""
+import sys
+
+stat = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+ctr = 0
+total_size = 0
+
+try:
+    for line in sys.stdin:
+        status_code = line.split('"')[2].split(" ")[1]
+        size = int(line.split('"')[2].split(" ")[2])
+        total_size += size
+        ctr += 1
+
+        for key in sorted(stat.keys()):
+            if status_code == key:
+                stat[key] += 1
+        if ctr == 10:
+            ctr = 0
+            print("File size: ", total_size)
+            for key in sorted(stat.keys()):
+                if stat[key]:
+                    print("{:s}: {:d}".format(key, stat[key]))
+
+except KeyboardInterrupt:
+    for key in sorted(stat.keys()):
+        if stat[key]:
+            print("{:s}: {:d}".format(key, stat[key]))
+    raise
---
+++ 0x06-log_parsing/README.md
@@ -0,0 +1,20 @@
+![Banner]()
+
+## Description
+
+Script that reads stdin line by line and computes metrics.
+
+---
+
+## Content
+#### 0. Log parsing --> [Script file.](./0-stats.py)
+
+---
+
+## Author
+#### Luz Sánchez Bolaños
+- Github: [zulsb](https://github.com/zulsb)
+- Twitter: [@LuzSanchezB](https://twitter.com/LuzSanchezB)
+
+Cohort 10 |
+Cali, Colombia 2020
