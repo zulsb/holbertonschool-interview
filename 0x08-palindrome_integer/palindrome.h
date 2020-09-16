835a7b3a357c686830f0b4cd2c0f78ab63ec172e
zulsb <zulsb2093@gmail.com>
2020-09-15 21:13:11 -0500

    Add header

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,10 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 0. Palindrome unsigned integer */
+int is_palindrome(unsigned long n);
+
+#endif /* PALINDROME_H */
