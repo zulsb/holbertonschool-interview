de7ba8c611202bc00932cbeee79ef29b83def7a7
zulsb <zulsb2093@gmail.com>
2020-09-23 22:43:08 -0500

    Validación UTF-8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains the method that determines represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """ Method that determines if a given data set represents a valid
+        UTF-8 encoding.
+        Args:
+        data: Represented by a list of integers.
+    """
+
+    validation = 0
+    for i in data:
+        i = format(i, "#010b")[-8:]
+        if validation != 0:
+            validation -= 1
+            if not i.startswith("10"):
+                return False
+        if i[0] == "1":
+            validation = len(i.split("0")[0])
+            if validation == 1 or validation > 4:
+                return False
+            validation -= 1
+
+    return True if validation == 0 else False
---
+++ 0x09-utf8_validation/README.md
@@ -0,0 +1,20 @@
+![]()
+
+## Description
+
+Method that determines if a given data set represents a valid UTF-8 encoding.
+
+---
+
+## Content
+#### 0. UTF-8 Validation --> [Method file.](./0-validate_utf8.py)
+
+---
+
+## Author
+#### Luz Sánchez Bolaños
+- Github: [zulsb](https://github.com/zulsb)
+- Twitter: [@LuzSanchezB](https://twitter.com/LuzSanchezB)
+
+Cohort 10 |
+Cali, Colombia 2020
