3deabec65250353386bbae77a033647d5959609f
zulsb <zulsb2093@gmail.com>
2020-09-23 23:29:49 -0500

    Validación UTF-8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -12,4 +12,13 @@ def validUTF8(data):
     """
 
     validation = 0
+    for i in data:
+        i = format(i, "#010b")[-8:]
+        if validation != 0:
+            validation -= 1
+        elif i[0] == "1":
+            if validation == 1 or validation > 4:
+                return False
+            validation -= 1
+
     return True if validation == 0 else False
