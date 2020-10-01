f59c733f529ebfb552a2ce6ae74ede6c3c999977
zulsb <zulsb2093@gmail.com>
2020-09-30 19:42:40 -0500

    Function that slides and merges an array of integers

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -70,13 +70,6 @@ void left(int *line, int size_aux)
 			}
 		}
 	}
-	if (j == 0)
-	{
-		line[j] = aux;
-		j++;
-	}
-	for (; j < size_aux; j++)
-		line[j] = 0;
 }
 
 /**
@@ -119,11 +112,4 @@ void right(int *line, int size_aux)
 			}
 		}
 	}
-	if (j == size_aux - 1)
-	{
-		line[j] = aux;
-		j--;
-	}
-	for (; j >= 0; j--)
-		line[j] = 0;
 }
