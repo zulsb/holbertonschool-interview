cc15fb856050e85303a83fb81f48e93c6eef10ed
zulsb <zulsb2093@gmail.com>
2020-10-28 21:34:06 -0500

    Nqueens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,7 +3,7 @@
 import sys
 
 
-def queens_potitions(N, row, queens):
+def queens_potitions(N, r, queens):
     """ The N queens puzzle is the challenge of placing
         N non-attacking queens on an N×N chessboard.
 
@@ -11,7 +11,7 @@ def queens_potitions(N, row, queens):
     for i in range(N):
         flag = 0
         for j in queens:
-            if abs(i - j[1]) == abs(row - j[0]):
+            if abs(i - j[1]) == abs(r - j[0]):
                 flag = 1
                 break
             if i == j[1]:
@@ -19,9 +19,9 @@ def queens_potitions(N, row, queens):
                 break
 
         if flag == 0:
-            queens.append([row, i])
-            if row != N - 1:
-                queens_potitions(N, row + 1, queens)
+            queens.append([r, i])
+            if r != N - 1:
+                queens_potitions(N, r + 1, queens)
             else:
                 print(queens)
             queens.pop()
@@ -29,7 +29,20 @@ def queens_potitions(N, row, queens):
 
 if __name__ == "__main__":
 
+    if len(sys.argv) > 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if isinstance(sys.argv[1], int):
+        print("N must be a number")
+        exit(1)
+
     N = int(sys.argv[1])
-    row = 0
+
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    r = 0
     queens = []
-    queens_potitions(N, row, queens)
+    queens_potitions(N, r, queens)
