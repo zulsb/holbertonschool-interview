14c57425f2b0fc1f160efe8a9f0e6d58d97c989c
zulsb <zulsb2093@gmail.com>
2020-11-25 19:52:47 -0500

    Rain

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Rain module.
+"""
+
+
+def rain(walls):
+    """ Function that calculates how much water will
+        be retained after it rains.
+        Arg:
+            walls: Is a list of non-negative integers.
+        Return:
+            Integer indicating total amount of rainwater retained.
+    """
+    preWall = 0
+    gap = []
+    retain = 0
+
+    for pos, w in enumerate(walls):
+        if len(walls) < 1:
+            return 0
+        if w != 0 and preWall != 0:
+            if w >= preWall:
+                retain += (preWall * len(gap)) - sum(gap)
+        if w != 0 and preWall == 0:
+            preWall = w
+        if w == 0 and preWall != 0:
+            gap.append(w)
+    return retain if retain != 0 else 1
