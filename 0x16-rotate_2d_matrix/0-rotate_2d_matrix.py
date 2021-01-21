d081eaadd225f0d577795906599c728bd4c9e16a
zulsb <zulsb2093@gmail.com>
2021-01-20 21:30:34 -0500

    Rotate 2D matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module that rotate 2D matrix.
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Method that rotate it 90 degrees clockwise.
+        Arg:
+            matrix: Have 2 dimensions.
+        Return:
+            Anything.
+    """
+
+    c = matrix.copy()
+
+    for iter in range(len(matrix[0])):
+        aux = []
+
+        for row in reversed(c):
+            aux.append(row[iter])
+
+        matrix[iter] = aux
