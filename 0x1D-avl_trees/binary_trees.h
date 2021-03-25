ddad1bc997a0be69250abeb602d695cee6ce8aba
zulsb <zulsb2093@gmail.com>
2021-03-24 23:09:07 -0500

    Header

---
+++ 0x1D-avl_trees/binary_trees.h
@@ -0,0 +1,45 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/**========================================================================*/
+/**-LIBRARIES--------------------------------------------------------------*/
+/**========================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/**========================================================================*/
+/**-STRUCTURE--------------------------------------------------------------*/
+/**========================================================================*/
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+
+/**========================================================================*/
+/**-PROTOTYPES-------------------------------------------------------------*/
+/**========================================================================*/
+
+/** 0. Is AVL */
+int binary_tree_is_avl(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
