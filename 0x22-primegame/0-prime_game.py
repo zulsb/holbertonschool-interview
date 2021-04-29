6ea6397c68340716bb68503ffb9ca75d97f1e3d6
zulsb <zulsb2093@gmail.com>
2021-04-28 19:28:18 -0500

    Prime Game

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -12,35 +12,45 @@ def isWinner(x, nums):
             If the winner cannot be determined, return None.
     """
 
-    prime_l = get_prime(nums)
+    s = {"Maria": 0, "Ben": 0}
+    primes = [0, 0, 2]
+    add_prime(max(nums), primes)
 
-    if prime_l > 0:
-        if prime_l % 2 == 0:
-            return "Ben"
+    for round in range(x):
+        _sum = sum((i != 0 and i <= nums[round])
+                   for i in primes[:nums[round] + 1])
+        if (_sum % 2):
+            winner = "Maria"
         else:
-            return "Maria"
+            winner = "Ben"
+        if winner:
+            s[winner] += 1
+
+    if s["Maria"] > s["Ben"]:
+        return "Maria"
+    elif s["Ben"] > s["Maria"]:
+        return "Ben"
+
     return None
 
 
-def get_prime(nums):
-    """ Method that get all prime numbers.
-        Args:
-            nums: Is an array of n.
-        Return: The total number of prime numbers.
+def add_prime(n, primes):
+    """ Method that all primes.
     """
 
-    prime_n = []
+    last_prime = primes[-1]
+    if n > last_prime:
+        for i in range(last_prime + 1, n + 1):
+            if is_prime(i):
+                primes.append(i)
+            else:
+                primes.append(0)
 
-    for num in nums:
-        if num == 1:
-            prime_n.append(num)
-        if num >= 2:
-            flag = 0
-            for iter in range(2, num):
-                if num % iter == 0:
-                    flag = 1
-                    break
-            if flag == 0:
-                prime_n.append(num)
-    set_prime = set(prime_n)
-    return len(set_prime)
+
+def is_prime(n):
+    """ Method taht check if n is a prime number.
+    """
+    for i in range(2, int(n ** 0.5) + 1):
+        if not n % i:
+            return False
+    return True
